31784fa5b376e80132f281f758c35bf588f0c4c0
parktkdrb <qkrtkdrb05@naver.com>
2024-10-17 17:18:10 +0900

    아이템 지속시간 업데이트

--- Assets/Scripts/PSG/Player/PlayerCollision.cs
+++ Assets/Scripts/PSG/Player/PlayerCollision.cs
@@ -6,7 +6,14 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     private StatHandler statHandler;
+    // 적 충돌 쿨타임
     private bool enemyCollisionCooldown = true;
+    //아이템 충돌 쿨타임
+    private bool ProjectilePowerCooldown = true;
+    private bool AttackSpeedCooldown = true;
+    private bool SpeedCooldown = true;
+    private bool ShieldnCooldown = true;
+
     private void Start()
     {
         statHandler = GetComponent<StatHandler>();
@@ -25,17 +32,35 @@ public class PlayerCollision : MonoBehaviour
         {
             ItemController itemController = collision.GetComponent<ItemController>();
             ItemSO collisionItemSO = itemController.itemSO;
-
-            statHandler.ProjectilePower += collisionItemSO.ProjectilePower;
-            statHandler.AttackSpeed -= collisionItemSO.AttackSpeed;
-            statHandler.Speed += collisionItemSO.Speed;
-            statHandler.Shield += collisionItemSO.Shield;
-            statHandler.point += collisionItemSO.point;
+            StartCoroutine(ApplyItemTime(collisionItemSO, collisionItemSO.CoolTime));
         }
     }
     private void TriggerCoolDown()
     {
         enemyCollisionCooldown = true;
         Debug.Log("쿨다운 해제");
+    }
+    private IEnumerator ApplyItemTime(ItemSO itemSO, float duration)
+    {
+        // 아이템 효과 적용
+        statHandler.ProjectilePower += itemSO.ProjectilePower;
+        statHandler.AttackSpeed -= itemSO.AttackSpeed;
+        statHandler.Speed += itemSO.Speed;
+        statHandler.Shield += itemSO.Shield;
+        statHandler.point += itemSO.point;
+
+        Debug.Log("아이템 효과 적용. 지속 시간: " + duration + "초");
+
+        // 주어진 시간(duration) 동안 대기
+        yield return new WaitForSeconds(duration);
+
+        // 원래 상태로 복구
+        statHandler.ProjectilePower -= itemSO.ProjectilePower;
+        statHandler.AttackSpeed += itemSO.AttackSpeed;
+        statHandler.Speed -= itemSO.Speed;
+        statHandler.Shield -= itemSO.Shield;
+        statHandler.point -= itemSO.point;
+
+        Debug.Log("아이템 효과 종료.");
     }
 }

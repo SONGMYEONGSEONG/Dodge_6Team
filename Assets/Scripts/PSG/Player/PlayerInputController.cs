3d301295950bc8fb313640bdd826360866e5e9db
SONGMYEONGSEONG <100818983+SONGMYEONGSEONG@users.noreply.github.com>
2024-10-20 13:27:29 +0900

    feat :
    1.Test : 멀티모드 테스트를 위한 player 객체 및 inputAction 세팅
    
    2.Update : 플레이어 조작 범위 확장 및 VritualCamera 추가로 카메라가  플레이어 추적 가능하게함

--- Assets/Scripts/PSG/Player/PlayerInputController.cs
+++ Assets/Scripts/PSG/Player/PlayerInputController.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerInputController : TopDownController
 {
+    [SerializeField] private int playerNum = 1;
     private Camera camera;
 
     InputActionAsset inputAction;
@@ -38,10 +39,26 @@ public class PlayerInputController : TopDownController
     public void OnLook(InputValue _value)
     {
         Vector2 newAim = _value.Get<Vector2>();
-        Vector2 worldPos = camera.ScreenToWorldPoint(newAim);
-        newAim = (worldPos - (Vector2)transform.position).normalized;
+        switch (playerNum)
+        {
+            case 1:
 
-        CallLookEvent(newAim);
+                Vector2 worldPos = camera.ScreenToWorldPoint(newAim);
+                newAim = (worldPos - (Vector2)transform.position).normalized;
+                CallLookEvent(newAim);
+                break;
+            case 2:
+                if (newAim != Vector2.zero)
+                {
+                    Debug.Log(newAim);
+                    newAim = newAim.normalized;
+                    CallLookEvent(newAim);
+                }
+                break;
+
+        }
+
+
     }
 
     public void OnFire(InputValue _value)

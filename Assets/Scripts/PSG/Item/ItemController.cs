c188530bb69924af8d7ca1b8f315432bedd78336
SONGMYEONGSEONG <100818983+SONGMYEONGSEONG@users.noreply.github.com>
2024-10-21 11:08:17 +0900

    Fix : 아이템 2개생성되는 버그 수정

--- Assets/Scripts/PSG/Item/ItemController.cs
+++ Assets/Scripts/PSG/Item/ItemController.cs
@@ -9,10 +9,26 @@ public class ItemController : MonoBehaviour , iPoolable<ItemController>
     public event Action<ItemController> OnEventPushObject;
 
     [SerializeField] public ItemSO _ItemSO;
+    [SerializeField] private float itemFalseDelay = 3.0f;
+
+    Coroutine itemFalseCoroutine;
+
+    private void OnEnable()
+    {
+        if (itemFalseCoroutine == null)
+        {
+            StartCoroutine(ItemActiveFalseDelay());
+        }
+        else
+        {
+            StopCoroutine(itemFalseCoroutine);
+            StartCoroutine(ItemActiveFalseDelay());
+        }
+       
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        StartCoroutine(ItemActiveFalseDelay());
         if (collision.CompareTag("Player"))
         {
             //오브젝트 푸쉬 코드 작성 
@@ -22,7 +38,7 @@ public class ItemController : MonoBehaviour , iPoolable<ItemController>
     }
     private IEnumerator ItemActiveFalseDelay()
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(itemFalseDelay);
 
         OnEventPushObject?.Invoke(this);
     }
